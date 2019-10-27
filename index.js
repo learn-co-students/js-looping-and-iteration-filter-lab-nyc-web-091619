0736b63f7161b44ba5ea19bd272c4c636603eb98
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-27 19:31:15 -0400

    met all specs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(coll, name) {
+  return coll.filter(function(collName) {
+    return collName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(coll, partial) {
+  return coll.filter(function(collName) {
+    return collName.slice(0, partial.length) === partial;
+  });
+}
+
+function matchName(coll, name) {
+  return coll.filter(function(collObj) {
+    return collObj.name === name;
+  });
+}
