009735f20c4be94ad0d8016b87036ac6235f598b
dan1mal <dreamer@panix.com>
2019-10-28 13:02:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(driver_array, search_string) {
+    return driver_array.filter(function(driver) {
+        return driver.toLowerCase().indexOf(search_string.toLowerCase()) !== -1;
+    })
+}
+
+function fuzzyMatch(driver_array, partial_string) {
+    let str_length = partial_string.length;
+    return driver_array.filter(function(driver) {
+        return driver.slice(0, str_length) === partial_string;
+    })
+}
+
+function matchName (driver_array, driver_name_element) {
+    return driver_array.filter(function (driver) {
+      return driver.name.toLowerCase() === driver_name_element.toLowerCase();
+    });
+  }
