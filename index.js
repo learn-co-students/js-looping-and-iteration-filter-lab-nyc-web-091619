4a3ee0cfac0f5fe0186565990cf26920527f77e4
Connor Meth <connormeth27@gmail.com>
2019-10-29 12:20:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(arr, name) {
+    return arr.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+     });
+}
+
+function fuzzyMatch(arr, namePartial) {
+    let nameLength = namePartial.length;
+    return arr.filter(function (driverName) {
+        return driverName.slice(0, nameLength) === namePartial.slice()
+    });
+}
+
+function matchName(arr, name) {
+    return arr.filter( function (driver){
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
